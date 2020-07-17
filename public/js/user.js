bf94e01c2b3d296a100ba58f0a2d0d10bfe1ed20
Felipe José de Góes <felipe92goes@gmail.com>
2020-07-16 23:10:08 -0300

    add funçoes js

---
+++ public/js/user.js
@@ -0,0 +1,129 @@
+function createUser(form,nome,email,senha,confirmacaoSenha)
+{
+    if (senha === confirmacaoSenha) {
+        if(findEmpty(form) === false)
+        {
+            url  = location.origin+'/api/users'
+            data = new FormData
+            data.append('nome'  ,nome)
+            data.append('email' ,email)
+            data.append('senha' ,senha)
+        
+            fetch(url,
+                { method: 'POST', body  : data })
+                .then(res => res.json())
+                .then(res => 
+                    {
+                        if (res.status == 'ok')
+                        {
+                            showNotification('Cliente cadastrado com sucesso, aguarde... Estamos atualizando','success',5000,true)
+                        }
+                        else
+                        {
+                            showNotification(res.message,'warning',5000)
+                        }
+                    })
+                .catch(err => console.log(err))
+        }
+    }
+    else
+    {
+        showNotification('A confirmação de senha não corresponde a senha','warning',5000)
+    }
+}
+
+function login(form,email,senha)
+{
+    if(findEmpty(form) === false)
+    {
+        url  = location.origin+'/api/login'
+        data = new FormData
+        data.append('email' ,email)
+        data.append('senha' ,senha)
+    
+        fetch(url,
+            { method: 'POST', body  : data })
+            .then(res => res.json())
+            .then(res => 
+                {
+                    if (res.status == 'ok')
+                    {
+                        showNotification('Seja bem-vindo!... Estamos atualizando','success',5000,true)
+                    }
+                    else
+                    {
+                        showNotification(res.message,'warning',5000)
+                    }
+                })
+            .catch(err => console.log(err))
+    }
+}
+
+function listUsers(form)
+{
+    if(findEmpty(form) === false)
+    {
+        url  = location.origin+'/api/users'
+    
+        fetch(url)
+            .then(res => res.json())
+            .then(res => 
+                {
+                    if (res.status == 'ok')
+                    {
+                        console.log(res)
+                    }
+                    else
+                    {
+                        showNotification(res.message,'warning',5000)
+                    }
+                })
+            .catch(err => console.log(err))
+    }
+}
+
+function getUser(form,id)
+{
+    if(findEmpty(form) === false)
+    {
+        url  = location.origin+'/api/users/'+id
+    
+        fetch(url)
+            .then(res => res.json())
+            .then(res => 
+                {
+                    if (res.status == 'ok')
+                    {
+                        console.log(res)
+                    }
+                    else
+                    {
+                        showNotification(res.message,'warning',5000)
+                    }
+                })
+            .catch(err => console.log(err))
+    }
+}
+
+function deleteUser(form,id)
+{
+    if(findEmpty(form) === false)
+    {
+        url  = location.origin+'/api/users/'+id
+    
+        fetch(url,{method:'DELETE'})
+            .then(res => res.json())
+            .then(res => 
+                {
+                    if (res.status == 'ok')
+                    {
+                        showNotification('Usuário apagado com sucesso!<br>... Estamos atualizando','success',5000,true)
+                    }
+                    else
+                    {
+                        showNotification(res.message,'warning',5000)
+                    }
+                })
+            .catch(err => console.log(err))
+    }
+}
---
+++ public/js/utils.js
@@ -0,0 +1,166 @@
+/** ****************************************************************************
+ * 
+ * 
+ * 
+ * @description executa a busca de dados via GET e Fetch API na url informada
+ * @tutorial https://github.github.io/fetch/  https://developer.mozilla.org/pt-BR/docs/Web/API/Fetch_API
+ * @param {String} urlToFetch 
+ * @param {Object} urlParams 
+ * @param {Function} actionOnSuccess 
+ * @param {Function} actionOnFailure 
+ */
+function fetchMethodGET(urlToFetch, urlParams, actionOnSuccess, actionOnFailure)
+{
+    let queryParams = builURLQuery(urlParams)
+
+    // console.log(urlToFetch + queryParams)
+
+    fetch(urlToFetch + queryParams)
+        .then(res => res.json())
+        .then(jsonRes =>
+        {
+            actionOnSuccess(jsonRes)
+        })
+        .catch(error =>
+        {
+            actionOnFailure(error)
+        })
+
+    /**
+     * @description monta a query string da opção consulta GET 
+     * @param {Object} object 
+     */
+    function builURLQuery(object)
+    {
+        return '?' + Object.entries(object)
+            .map(pair => pair.map(encodeURIComponent).join('='))
+            .join('&')
+    }
+}
+
+/** ****************************************************************************
+ * 
+ * 
+ * @description executa a busca de dados via POST e Fetch API na url informada
+ * @tutorial https://github.github.io/fetch/  https://developer.mozilla.org/pt-BR/docs/Web/API/Fetch_API
+ * @param {String} urlToFetch 
+ * @param {FormData} formData 
+ * @param {Function} actionOnSuccess 
+ * @param {Function} actionOnFailure 
+ */
+function fetchMethodPOST(urlToFetch, formData, actionOnSuccess, actionOnFailure)
+{
+    fetch(urlToFetch,
+        {
+            method: 'POST',
+            body: formData
+        })
+        .then(res => res.json())
+        .then(jsonRes =>
+        {
+            actionOnSuccess(jsonRes)
+        })
+        .catch(error =>
+        {
+            actionOnFailure(error)
+        })
+}
+
+/******************************************************************************/
+
+/**
+ * 
+ * Função para verificar inputs, selects e textareas vazias
+ * 
+ * @param {HTMLElement} parent 
+ * @returns {String} 
+ */
+function findEmpty(parent)
+{
+    let inputs     = parent.getElementsByTagName('input')    
+    let selects    = parent.getElementsByTagName('select')   
+    let textareas  = parent.getElementsByTagName('textarea')
+    let isEmpty    = [] 
+    
+    if (inputs.length !== 0)
+    {
+        for (input of inputs)
+        {
+            if(input.value == '' && input.required == true)
+            {
+                input.classList.add('uk-form-danger')
+                isEmpty.push(input) 
+            }
+            else
+            {
+                input.classList.remove('uk-form-danger')
+            }
+        }
+    }
+    
+    if (selects.length !== 0)
+    {
+        for (select of selects)
+        {
+            if(select.value == '' && select.required == true)
+            {
+                select.classList.add('uk-form-danger')
+                isEmpty.push(select) 
+            }
+            else
+            {
+                select.classList.remove('uk-form-danger')
+            }
+        }
+    }
+   
+    if (textareas.length !== 0)
+    {
+        for (textarea of textareas)
+        {
+            if(textarea.value == '' && textarea.required == true)
+            {
+                textarea.classList.add('uk-form-danger')
+                isEmpty.push(textarea) 
+            }
+            else
+            {
+                textarea.classList.remove('uk-form-danger')
+            }
+        }
+    }
+
+    if(isEmpty.length !== 0)
+    {
+        UIkit.notification("Existem campos obrigatórios não preenchidos", 
+                    {
+                        status: 'danger', 
+                        pos: 'top-center',
+                        timeout: 5000
+                    });
+    }
+
+    return isEmpty.length !== 0 ? true : false
+}
+
+/**
+ * 
+ * @param {String} type primary | success | warning | danger
+ * @param {String} message 
+ * @param {Number} time
+ * @param {Boolean} reload 
+ */
+function showNotification(message, type='primary', time=5000, reload=false)
+{
+    UIkit.notification({
+        message: message,
+        status: type,
+        pos: 'top-center',
+        timeout: time
+    });
+
+    if (reload===true) 
+    {
+        location.reload();
+    }
+}
